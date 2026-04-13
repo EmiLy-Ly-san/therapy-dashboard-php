65bd4f53c238620811a92f1aa1a7c05bbe6d658c
EmiLy-Ly-san <emilie.deduyver@gmail.com>
2026-04-13 21:53:28 +0200

    index.php updated with CRSF token

--- public/index.php
+++ public/index.php
@@ -3,10 +3,30 @@
 
 declare(strict_types=1);
 
+session_start();
+
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../config/database.php';
 
 use App\Controller\NoteController;
+
+
+// CSRF TOKEN
+
+if (empty($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+
+function checkCsrf(): void
+{
+    $sessionToken = $_SESSION['csrf_token'] ?? '';
+    $postToken = $_POST['csrf_token'] ?? '';
+
+    if (!hash_equals($sessionToken, $postToken)) {
+        http_response_code(403);
+        exit('Token CSRF invalide');
+    }
+}
 
 // paramètres de route
 $page = $_GET['page'] ?? 'notes';
@@ -19,7 +39,7 @@ $pdo = getConnection();
 // Instanciation du controller
 $controller = new NoteController($pdo);
 
-// Routage 
+// Routage
 match ($page . '/' . $action) {
     'notes/list'   => $controller->list(),
     'notes/show'   => $controller->show($id),
